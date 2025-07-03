ca524505f935b1d69f7855dd5cc6a3d93b4789a4
rioreo9 <miotomine@gmail.com>
2025-07-03 15:30:07 +0900

    インタラクトできるオブジェクトの仕組み変更 主にインターフェースのメソッドや基底クラスの変更

--- Assets/Scripts/InteractObjectScripts/BaseInteractObject.cs
+++ Assets/Scripts/InteractObjectScripts/BaseInteractObject.cs
@@ -1,34 +1,22 @@
 using Fusion;
 
-using UnityEngine;
-
-public abstract class BaseInteractObject : NetworkBehaviour, IInteractObject
+/// <summary>
+/// 峯廻制作
+/// 操縦・操作できるオブジェクトの基底クラス(主に船についているタレットなど)
+/// </summary>
+public abstract class BaseInteractObject : NetworkBehaviour, IInteractableControllable
 {
+    // インタラクトできるかどうかのフラグ
     protected bool _isInteractable = false;
 
-    protected bool _isButtonPressed = false;
-
-    protected Vector2 _InputXY = default;
-
+    /// <summary>
+    /// インタラクトできるかどうかのフラグ
+    /// </summary>
     public bool IsInteractable { get => _isInteractable; set => _isInteractable = value; }
 
-    public bool IsButtonPressed { get => _isButtonPressed; set => _isButtonPressed = value; }
-
-    public Vector2 InputXY { get => _InputXY; set => _InputXY = value; }
-
-    public abstract void ControlInteractObject();
-}
-
-
-public interface IInteractObject
-{
     /// <summary>
-    /// インタラクトできるかどうかを示すプロパティ
+    /// そのオブジェクトをコントロールするためのメソッド
     /// </summary>
-    public bool IsInteractable { get; set; }
-
-    public bool IsButtonPressed { get; set; }
-
-
-  
+    /// <param name="networkInput">Networkに対応したInput</param>
+    public abstract void ControlObject(NetworkInput networkInput);
 }
