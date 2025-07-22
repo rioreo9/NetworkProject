5649da13ca8690c3bcaf2d0c2bab9f5bce2b82be
wakame3255 <46024199+wakame3255@users.noreply.github.com>
2025-07-22 10:46:14 +0900

    ゲーム管理システムの構築開始

---
+++ Assets/Scripts/Core/GameFlowHandler.cs
@@ -0,0 +1,18 @@
+using System;
+using R3;
+using Fusion;
+
+public enum GameState
+{
+    WaitingForPlayers,
+    Preparation,
+    WaveAction,
+    WaveComplete,
+    UpgradePhase,
+    GameOver,
+    Victory
+}
+public class GameFlowHandler : NetworkBehaviour
+{
+    
+}
