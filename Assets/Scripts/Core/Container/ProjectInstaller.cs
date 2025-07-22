ba52e4191066eeeecc9de27b75c53df1954e0e79
wakame3255 <okeitan325@gmail.com>
2025-07-23 00:26:58 +0900

    構成の更新、DIコンテナによる依存関係の解決導入

--- Assets/Scripts/Core/Container/ProjectInstaller.cs
+++ Assets/Scripts/Core/Container/ProjectInstaller.cs
@@ -1,6 +1,30 @@
 using Fusion;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
 
-public class ProjectInstaller : NetworkBehaviour
+public class ProjectInstaller : LifetimeScope
 {
-  
+    [SerializeField, Required]
+    private GameFlowHandler _gameFlowHandler;
+    [SerializeField, Required]
+    private EnemyWaveHandler _enemyWaveHandler;
+
+    protected override void Configure(IContainerBuilder builder)
+    {
+        // インスタンス登録（シーンに配置されたオブジェクトを使用）
+        if (_gameFlowHandler != null)
+        {
+            builder.RegisterInstance(_gameFlowHandler).As<IGameStateNotice>();
+        }
+
+        if (_enemyWaveHandler != null)
+        {
+            builder.RegisterInstance(_enemyWaveHandler);
+        }
+
+        // または、型による自動登録の場合
+        // builder.RegisterComponentInHierarchy<GameFlowHandler>().As<IGameStateNotice>();
+        // builder.RegisterComponentInHierarchy<EnemyWaveHandler>();
+    }
 }
--- Assets/Scripts/Core/EnemyWaveHandler.cs
+++ Assets/Scripts/Core/EnemyWaveHandler.cs
@@ -1,9 +1,46 @@
 using Fusion;
-using System;
-using System.Runtime.InteropServices;
+using VContainer;
+using UnityEngine;
+using VContainer.Unity;
 
 public class EnemyWaveHandler : NetworkBehaviour
 {
-    [InterfaceType(typeof(IGameStateNotice))]
     private IGameStateNotice _gameStateNotice;
+
+    public override void Spawned()
+    {
+        // ネットワークオブジェクトがスポーンされた時に手動で依存性注入を実行
+        InjectDependencies();
+    }
+
+    /// <summary>
+    /// VContainerから手動で依存性を注入するメソッド
+    /// </summary>
+    private void InjectDependencies()
+    {
+        // LifetimeScopeを検索してコンテナを取得
+        LifetimeScope lifetimeScope = FindFirstObjectByType<LifetimeScope>();
+        if (lifetimeScope != null)
+        {
+            // コンテナから依存性を解決
+            _gameStateNotice = lifetimeScope.Container.Resolve<IGameStateNotice>();
+            Debug.Log("EnemyWaveHandler: 依存性注入が完了しました");
+        }
+        else
+        {
+            Debug.LogError("EnemyWaveHandler: LifetimeScopeが見つかりません");
+        }
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (_gameStateNotice == null)
+        {
+            Debug.LogError("IGameStateNotice is not injected into EnemyWaveHandler.");
+        }
+        else
+        {
+            Debug.Log("EnemyWaveHandler spawned with IGameStateNotice injected.");
+        }
+    }
 }
