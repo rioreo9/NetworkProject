e325ef901064c7370798605114b94901714b1033
M <sakurasaitojs@gmail.com>
2025-08-14 20:06:09 +0900

    オブジェクトプール実装完了：シーンに追加

--- Assets/Scripts/NetWork/PoolObjectProvider.cs
+++ Assets/Scripts/NetWork/PoolObjectProvider.cs
@@ -5,57 +5,71 @@ using UnityEngine;
 
 public class PoolObjectProvider : Fusion.Behaviour, INetworkObjectProvider
 {
-    /// <summary>
-    /// If enabled, the provider will delay acquiring a prefab instance if the scene manager is busy.
-    /// </summary>
+	// 有効にすると、SceneMangerが処理中の場合にプレハブ取得を遅らせる
     [InlineHelp]
     public bool DelayIfSceneManagerIsBusy = true;
-   
-    /// <summary>
-    /// If enabled, the provider will disable object when they return to the pool. If disabled they will keep enabled and the material will be changed (This is for easy visualization only, not a good practice)
-    /// </summary>
-    private bool _disableObjectInPool = true;
+
+	// Inspectorで指定したPrefabのみObjectPool化できる
+	[SerializeField]
+    [Header("指定したプレハブのみプール化可能")]
+	private List<NetworkObject> _pooledWhiteListPrefabs = new List<NetworkObject>();
+
+
+
+	/// <summary>
+	/// プレハブIDごとに利用可能なNetworkObjectのキューを管理するディクショナリ
+	/// オブジェクトプールから再利用可能なオブジェクトを効率的に取得するために使用
+	/// </summary>
+	private Dictionary<NetworkPrefabId, Queue<NetworkObject>> _pooledObjectsDict = new Dictionary<NetworkPrefabId, Queue<NetworkObject>>();
+
+
+
+	/// プール対象と判断したPrefabId を記録：Releaseで使用
+	private HashSet<NetworkPrefabId> _pooledPrefabIds = new HashSet<NetworkPrefabId>();
     
-    private Dictionary<NetworkPrefabId, Queue<NetworkObject>> _free = new Dictionary<NetworkPrefabId, Queue<NetworkObject>>();
-    
+
     /// <summary>
-    /// How many objects are going to be kept on the pools, 0 or negative means to pool all released objects.
+	/// プールに保持する最大数0以下は解放済みの全オブジェクトをプールします
     /// </summary>
+    [SerializeField]
+    [Header("プールの最大数、0以下だと上限なし")]
     private int _maxPoolCount = 0;
-    
-    /// The base <see cref="NetworkObjectProviderDefault"/> by default simply instantiate a new game object.
-    /// Let's create a method to use a custom logic that will pool objects.
-    protected NetworkObject InstantiatePrefab(NetworkRunner runner, NetworkObject prefab,
-        NetworkPrefabId contextPrefabId)
+
+
+
+
+
+
+
+
+    /// <summary>
+    /// オブジェクトプール用のオブジェクト生成 キューに挿入
+    /// 既定では <see cref="NetworkObjectProviderDefault"/>
+    /// </summary>
+    protected NetworkObject InstantiatePrefab(NetworkRunner runner, NetworkObject prefab,  NetworkPrefabId contextPrefabId)
     {
         var result = default(NetworkObject);
         
-        // Found free queue for prefab AND the queue is not empty. Return free object.
-        if (_free.TryGetValue(contextPrefabId, out var freeQ))
+		// プールからオブジェクトを取得を試行
+        if (_pooledObjectsDict.TryGetValue(contextPrefabId, out var freeQ))
         {
+
+            // キューに利用可能なオブジェクトがある場合は再利用
             if (freeQ.Count > 0)
             {
                 result = freeQ.Dequeue();
                 result.gameObject.SetActive(true);
-                
-                // ----------------------------------------------------------------------
-                // Make rigidbody non-kinematic again. Specific for this sample, if copying this object provider remove it.
-                if (result.gameObject.TryGetComponent<Rigidbody>(out var rigidbody))
-                {
-                    rigidbody.isKinematic = false;
-                    rigidbody.detectCollisions = true;
-                }
-                // ----------------------------------------------------------------------
                 
                 return result;
             }
         }
         else
         {
-            _free.Add(contextPrefabId, new Queue<NetworkObject>());
+            // 新しいプレハブID用の空キューを作成
+            _pooledObjectsDict.Add(contextPrefabId, new Queue<NetworkObject>());
         }
-        
-        // -- At this point a free queue was not yet created or were empty. Create new object.
+
+        // プールが空または未作成の場合は新規インスタンスを生成
         result = Instantiate(prefab);
         
         return result;
@@ -63,94 +77,130 @@ public class PoolObjectProvider : Fusion.Behaviour, INetworkObjectProvider
 
     protected void DestroyPrefabInstance(NetworkRunner runner, NetworkPrefabId prefabId, NetworkObject instance)
     {
-        // No free queue for this prefab OR the pool already have the max amount of object we defined. Should be destroyed.
-        if (_free.TryGetValue(prefabId, out var freeQ) == false || (_maxPoolCount > 0 && freeQ.Count >= _maxPoolCount))
+		// 該当プレハブの空きキューがない、またはプールが上限数に達している場合、Destroy()
+        if (_pooledObjectsDict.TryGetValue(prefabId, out var freeQ) == false || (_maxPoolCount > 0 && freeQ.Count >= _maxPoolCount))
         {
             Destroy(instance.gameObject);
             return;
         }
-        
-        
-        // Free queue found. Should cache.
+
+
+        // キューに空きががあるためプールに戻し、オブジェクトを非アクティブにする
         freeQ.Enqueue(instance);
-
-        if (_disableObjectInPool)
-        {
-            // Make objects inactive.
-            instance.gameObject.SetActive(false);
-        }
-        else
-        {
-            // ----------------------------------------------------------------------
-            // Make object transparent and kinematic. Specific for this sample, if copying this object provider remove it.
-            if (instance.gameObject.TryGetComponent<Renderer>(out var renderer))
-            {
-                var color = renderer.material.color;
-                color.a = .25f;
-                renderer.material.color = color;
-            }
-
-            if (instance.gameObject.TryGetComponent<Rigidbody>(out var rigidbody))
-            {
-                rigidbody.isKinematic = true;
-                rigidbody.detectCollisions = false;
-            }
-            // ----------------------------------------------------------------------
-        }
+		instance.gameObject.SetActive(false);
     }
 
+
+
+
+
+    /// <summary>
+    /// ネットワークオブジェクトのインスタンスを取得します
+    /// ホワイトリストに登録されたプレハブはオブジェクトプールから取得し、
+    /// それ以外は通常のInstantiateで新規作成します
+    /// </summary>
+    /// <param name="runner">NetworkRunner</param>
+    /// <param name="context">プレハブ取得のコンテキスト情報</param>
+    /// <param name="instance">取得されたNetworkObjectインスタンス</param>
+    /// <returns>取得結果（Success/Retry/Failed）</returns>
     public NetworkObjectAcquireResult AcquirePrefabInstance(NetworkRunner runner, in NetworkPrefabAcquireContext context,
         out NetworkObject instance)
     {
         
         instance = null;
 
+        // SceneManagerが処理中の場合は取得を遅延、リトライ
         if (DelayIfSceneManagerIsBusy && runner.SceneManager.IsBusy) {
             return NetworkObjectAcquireResult.Retry;
         }
 
+        // プレハブの読み込みを試行
         NetworkObject prefab;
         try {
             prefab = runner.Prefabs.Load(context.PrefabId, isSynchronous: context.IsSynchronous);
         } catch (Exception ex) {
+            // エラーの場合 Failed送信
             Log.Error($"Failed to load prefab: {ex}");
             return NetworkObjectAcquireResult.Failed;
         }
 
-        if (!prefab) {
-            // this is ok, as long as Fusion does not require the prefab to be loaded immediately;
-            // if an instance for this prefab is still needed, this method will be called again next update
-            return NetworkObjectAcquireResult.Retry;
-        }
+		// プレハブが読み込まれていない場合はリトライ
+		if (!prefab) {
 
-        instance = InstantiatePrefab(runner, prefab, context.PrefabId);
+
+			// Fusion が即時の読み込みを要求しない限り問題ありません
+			// まだ必要であれば次の更新で再度呼ばれます
+			return NetworkObjectAcquireResult.Retry;
+		}
+
+		// ホワイトリストに登録されているかチェック
+		bool shouldPool = _pooledWhiteListPrefabs.Contains(prefab);
+
+		if (shouldPool)
+		{
+			// プール対象として記録し、オブジェクトプールから取得
+			_pooledPrefabIds.Add(context.PrefabId);
+			instance = InstantiatePrefab(runner, prefab, context.PrefabId);
+		}
+		else
+		{
+			// プール対象外は通常のInstantiateで新規作成
+			instance = Instantiate(prefab);
+		}
         Assert.Check(instance);
+
 
         if (context.DontDestroyOnLoad) {
             runner.MakeDontDestroyOnLoad(instance.gameObject);
         } else {
+            // NetworkRunnerが管理するシーンに移動
             runner.MoveToRunnerScene(instance.gameObject);
         }
 
+        // Fusionにインスタンスを登録
         runner.Prefabs.AddInstance(context.PrefabId);
         return NetworkObjectAcquireResult.Success;
     }
 
+    /// <summary>
+    /// ネットワークオブジェクトのインスタンスを解放
+    /// プール対象のオブジェクトはプールに戻し、それ以外は破棄
+    /// Fusionから呼び出されるオブジェクトの解放処理
+    /// </summary>
+    /// <param name="runner">NetworkRunner</param>
+    /// <param name="context">オブジェクト解放のコンテキスト情報</param>
     public void ReleaseInstance(NetworkRunner runner, in NetworkObjectReleaseContext context)
     {
         var instance = context.Object;
 
-        // Only pool prefabs.
-        if (!context.IsBeingDestroyed) {
-            if (context.TypeId.IsPrefab) {
-                DestroyPrefabInstance(runner, context.TypeId.AsPrefabId, instance);
-            }
-            else
-            {
-                Destroy(instance.gameObject);
-            }
-        }
+		// オブジェクトが破棄中でない場合のみ処理を実行
+		if (!context.IsBeingDestroyed) {
 
+			// プレハブタイプのオブジェクトかチェック
+			if (context.TypeId.IsPrefab) {
+
+				var prefabId = context.TypeId.AsPrefabId;
+
+				// プール対象として登録されているかチェック
+				if (_pooledPrefabIds.Contains(prefabId))
+				{
+					// プール対象の場合はプールに戻すか破棄
+					DestroyPrefabInstance(runner, prefabId, instance);
+				}
+				else
+				{
+					// プール対象外は即座に破棄
+					Destroy(instance.gameObject);
+				}
+			}
+			else
+			{
+				// プレハブでないオブジェクトは即座に破棄
+				Destroy(instance.gameObject);
+			}
+		}
+
+        // Fusionからインスタンスを除去
         if (context.TypeId.IsPrefab) {
             runner.Prefabs.RemoveInstance(context.TypeId.AsPrefabId);
         }
@@ -159,18 +209,6 @@ public class PoolObjectProvider : Fusion.Behaviour, INetworkObjectProvider
     public NetworkPrefabId GetPrefabId(NetworkRunner runner, NetworkObjectGuid prefabGuid)
     {
         return runner.Prefabs.GetId(prefabGuid);
-    }
-
-    private void DisableObjectsAsDefaultReleaseBehaviour(bool value)
-    {
-        _disableObjectInPool = value;
-    }
-
-    public bool SwitchDefaultReleaseBehaviour()
-    {
-        DisableObjectsAsDefaultReleaseBehaviour(!_disableObjectInPool);
-
-        return _disableObjectInPool;
     }
 
     public void SetMaxPoolCount(int count)
